42c863fdca595d3572c5bd44c606609404de930b
Reggie Quiroz <regqc@hotmail.com>
2026-06-09 18:23:55 -0300

    Add env variables

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,23 +1,35 @@
 import express from "express";
 import path from "node:path";
 
+interface Options {
+  PORT: number;
+  PUBLIC_PATH: string;
+}
+
 export class Server {
+  constructor(private readonly options: Options) {
+    this.options = options;
+  }
+
   private app = express();
 
   async start() {
     // * Middlewares
 
     // * Public Folder
-    this.app.use(express.static("public"));
+    this.app.use(express.static(this.options.PUBLIC_PATH));
 
     this.app.get("/{*splat}", (req, res) => {
-      const indexPath = path.join(__dirname, "../../public/index.html");
+      const indexPath = path.join(
+        __dirname,
+        `../../${this.options.PUBLIC_PATH}/index.html`,
+      );
       res.sendFile(indexPath);
     });
 
     // console.log("Server started");
-    this.app.listen(8080, () => {
-      console.log(`Server runnin on port ${8080}`);
+    this.app.listen(this.options.PORT, () => {
+      console.log(`Server runnin on port ${this.options.PORT}`);
     });
   }
 }
