7193b68825438f9967367bfddc618ad08d213b60
Reggie Quiroz <regqc@hotmail.com>
2026-06-09 18:00:13 -0300

    Use previous project to set  express

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import path from "node:path";
 
 export class Server {
   private app = express();
@@ -8,6 +9,11 @@ export class Server {
 
     // * Public Folder
     this.app.use(express.static("public"));
+
+    this.app.get("/{*splat}", (req, res) => {
+      const indexPath = path.join(__dirname, "../../public/index.html");
+      res.sendFile(indexPath);
+    });
 
     // console.log("Server started");
     this.app.listen(8080, () => {
